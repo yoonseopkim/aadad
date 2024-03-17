e01bfec4375065b4cb9add026e36796fe05abb7e
yoonseopkim <qc70045@gmail.com>
2024-03-17 18:53:37 +0900

    ㅇ1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+import bg from './bg.png'
+import data from "./data";
+import {useState} from "react";
+import Card from "./Card";
 function App() {
+    let [shoes] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+
+
+
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+
+
+
+          <div>
+              <div className="main-bg" style={{ backgroundImage : 'url(' + bg + ')' }}></div>
+          </div>
+
+
+
+
+          {/*가로 3등분*/}
+          <div className="container">
+              <div className="row">
+
+                  <Card shoes={shoes[0]} i={1} />
+                  <Card shoes={shoes[1]} i={2} />
+                  <Card shoes={shoes[2]} i={3} />
+
+
+
+              </div>
+          </div>
+      </div>
   );
 }
 
